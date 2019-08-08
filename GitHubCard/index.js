bf8af0d7686bce353ccd31116c78660fcf47600d
Connor Holly <connorholly@Connors-MacBook-Pro-2.local>
2019-08-08 13:51:55 -0600

    get function right

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,9 +3,24 @@
            https://api.github.com/users/<your name>
 */
 
+axios.get('https://api.github.com/users/connorholly11')
+  .then((response) => {
+    console.log(response)
+
+    response.data.message.forEach ( item => {
+      let newPerson = userCard(item);
+      entrypoint.appendChild(newPerson);
+    })
+})
+    .catch((err) => {
+      console.log(err)
+  })
+
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
+
+
 
    Skip to Step 3.
 */
@@ -27,8 +42,8 @@
 const followersArray = [];
 
 /* Step 3: Create a function that accepts a single object as its only argument,
-          Using DOM methods and properties, create a component that will return the following DOM element:
-
+          Using DOM methods and properties, create a component that will return the following DOM element:*/
+/*
 <div class="card">
   <img src={image url of user} />
   <div class="card-info">
@@ -45,6 +60,22 @@ const followersArray = [];
 </div>
 
 */
+
+const card = document.querySelector('.cards')
+
+function createCards(){
+  const card1 = document.createElement('div');
+  const img = document.createElement('img');
+  const cardInfo = document.createElement('div');
+  const name = document.createElement('h3');
+  const username = document.createElement('p');
+  const location = document.createElement('p');
+  const profile = document.createElement('p');
+  const LinkGithub = document.createElement('a');
+  const followers = document.createElement('p');
+  const following = document.createElement('p');
+  const bio = document.createElement('p');
+}
 
 /* List of LS Instructors Github username's: 
   tetondan
